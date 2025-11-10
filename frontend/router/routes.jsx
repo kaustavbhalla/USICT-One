8b4c9200484554a9407930901af6057c4b2f08cc
SunjeetKajla <sunjeetkajla@gmail.com>
2025-11-10 23:30:42 +0530

    updated to latest NextJS 16 and Tialwind v4

---
+++ frontend/router/routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+export default function routes() {
+  return (
+    <>
+        {/* In Case we need to add more routes in the future */}
+    </>
+  )
+}
